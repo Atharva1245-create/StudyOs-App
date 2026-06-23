397b7c11331bd846808787aac99c063c29719845
Atharva <atharvakarbele12@gmail.com>
2026-06-23 18:26:03 +0530

    Added PWA support configuraton

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'StudyOs Productivity App',
+        short_name: 'StudyOs',
+        description: 'Frontend Student Productivity Tool',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3596/3596080.png', // Temporary App Icon
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3596/3596080.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
